cc1f1b0ada1b5326585d1ec74787e45ad9fc4dfa
Mc Nigno <mcnigno@gmail.com>
2019-11-17 06:20:54 +0100

    output files renamed

--- app/helpers.py
+++ app/helpers.py
@@ -13,7 +13,7 @@ def gen_file(index_rows):
 
 
 def index_init():
-    file = load_workbook('app/templates/079254C-0000-ML-100_0 - SECT. 5.xlsx')
+    file = load_workbook('app/templates/079254C-0000-ML-100_0.xlsx')
     sheet_list = file.sheetnames
     
     new_file = Workbook()
@@ -28,6 +28,10 @@ def index_init():
         sec1 = sheet['B13'].value
         sec2 = sheet['B15'].value
         sec3 = sheet['B17'].value
+        if sheet['B17'].value is not None:
+                para2 = sec3.split(' ')[0]
+        else:
+                para2 = sec2.split(' ')[0]
         for row in sheet.iter_rows(min_row=2):
             cell = row[2]
             if cell.value is not None and cell.value[0:7] == '079254C':
@@ -37,17 +41,17 @@ def index_init():
                         #location='5.1.1/' + cell.value + "_" + row[3].value,
                         display='something')
                 '''
-                cell_start_value = cell.value
+                cell_start_value = str(cell.value)
                 #cell.value = '=HYPERLINK("5.1.1\\' + cell_start_value + "_" + row[3].value +'"'+',"' + cell.value + '")'
                 #cell.value = "=HYPERLINK('5.1.1/" + cell.value + "_" + row[3].value + "','" + cell.value + "')"
                 cell.hyperlink = Hyperlink('link',
-                        target='.\\5.1.1\\' + cell_start_value + "_" + row[3].value,
+                        target='.\\FILE\\'+ para2 +'\\' + str(cell_start_value) + "_" + str(row[3].value),
                         #location='5.1.1/' + cell.value + "_" + row[3].value,
                         display='something')
                 print(cell.value)
                 link1 = row[3]
                 link1.hyperlink = Hyperlink('link',
-                        target='.\\5.1.1\\' + cell_start_value + "_" + row[3].value,
+                        target='.\\FILE\\'+ para2 +'\\' + cell_start_value + "_" + str(row[3].value),
                         #location='5.1.1/' + cell.value + "_" + row[3].value,
                         display='something')
                 
@@ -57,18 +61,43 @@ def index_init():
                 except:
                         new_ws.append((sec1,sec2,sec3,row[1].value,cell_start_value,row[3].value))  
 
-    file.save('test2.xlsx')
-    new_file.save('testof1.xlsx')
+    file.save('tavole.xlsx')
+    new_file.save('report_cliente.xlsx')
     '''
     f = gen_file(index_rows)
     
     return send_file(f, as_attachment=True, attachment_filename='indexmain.html')
     '''
- #index_init()
-
+#index_init()  
+ 
 def dict_doc():
         wb = load_workbook('testof1.xlsx')
         ws = wb.active
 
-        for row in ws.iter_rows:
-                
+        #for row in ws.iter_rows:
+
+
+import os
+
+def filelist():
+        new_file = Workbook()
+        listafolder = new_file.active
+        for path, folder, file in os.walk('./files'):
+                if isinstance(folder, list):
+                        for f in folder:
+                                listafolder.append([f])
+                else:
+                        listafolder.append([folder])
+        new_file.save('listafolder.xlsx')
+        
+        new_file = Workbook()
+        listafile = new_file.active
+        for path, folder, file in os.walk('./files'):
+                if isinstance(file, list):
+                        for f in file:
+
+                                listafile.append([path,f.split('.')[0],f.split('.')[1]])
+                else:
+                        listafile.append([path,file.split('.')[0],file.split('.')[1]])
+        new_file.save('listafile.xlsx') 
+#filelist()         
